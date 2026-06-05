ae646661681f4168d3db8e42339457b6aa279f89
unknown <bayashi.x28@gmail.com>
2026-06-05 09:18:35 +0800

    REFACTORED FRONTEND PT.2

--- frontend/src/Exporter/Hooks_Exporter.tsx
+++
@@ -1,3 +0,0 @@
-export { default as UseScreenWidth } from '../Hooks/ScreenWidth'
-export { default as AddPageTitle } from '../Hooks/AddPageTitle'
-export { default as AddClassBody } from '../Hooks/AddClassBody'
--- frontend/src/Hooks/AddPageTitle.tsx
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react'
-
-export default function AddPageTitle( title: string ) {
-    useEffect(() => {
-        document.title = title;
-    }, [ title ]);
-}
